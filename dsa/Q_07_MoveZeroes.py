80d4f75152606aff90635a571548d674f36ea2ce
ADITHYA UBALE <adithyaubale2006@gmail.com>
2026-08-22 20:43:06 +0530

    Update Q_07_MoveZeroes.py

--- dsa/Q_07_MoveZeroes.py
+++ dsa/Q_07_MoveZeroes.py
@@ -9,3 +9,25 @@ def Move(nums):
             
 nums = [5, 2, 0, 0, 88, 0, 10]
 print(Move(nums))
+
+#Method 2
+nums = [3, 0, 2, 5, 0, 0, 1]
+if len(nums) == 1:
+    return
+
+i = 0
+while i < len(nums):
+    if nums[i] == 0:
+        break
+    i += 1
+
+if i == len(nums):
+    return
+
+j = i + 1
+
+while j < len(nums):
+    if nums[j] != 0:
+        nums[i], nums[j] = nums[j], nums[i]
+        i += 1
+    j += 1
